7ddefa29181143f247746027a8befe02a44747a2
SachinOswal73 <sachin8973@gmail.com>
2022-12-08 17:40:02 +0530

    take it to remote repo

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function addTwoNum(firstNum, lastNum) {
+    var addResult = firstNum + lastNum;
+    // document.write(addResult);
+    console.log(addResult);
+}
+
+addTwoNum(50,25);
+// document.write(" | ");
+addTwoNum(35,95);
+
+
+function areaOfCircle(radius) {
+    return 3.14 * radius * radius;
+}
+var circle25 = areaOfCircle(10);
